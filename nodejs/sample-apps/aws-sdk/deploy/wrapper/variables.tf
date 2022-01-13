1a6a3648b64031e457abe28132f73b1bbce18723
Mateusz Rumian <mrumian@sumologic.com>
2022-01-13 14:29:18 +0100

    chore: update nodejs terraform

--- nodejs/sample-apps/aws-sdk/deploy/wrapper/variables.tf
+++ nodejs/sample-apps/aws-sdk/deploy/wrapper/variables.tf
@@ -1,29 +1,34 @@
 variable "name" {
   type        = string
   description = "Name of created function and API Gateway"
-  default     = "hello-nodejs-awssdk"
-}
-
-variable "collector_layer_arn" {
-  type        = string
-  description = "ARN for the Lambda layer containing the OpenTelemetry collector extension"
-  // TODO(anuraaga): Add default when a public layer is published.
-}
-
-variable "sdk_layer_arn" {
-  type        = string
-  description = "ARN for the Lambda layer containing the OpenTelemetry NodeJS Wrapper"
-  // TODO(anuraaga): Add default when a public layer is published.
-}
-
-variable "tracing_mode" {
-  type        = string
-  description = "Lambda function tracing mode"
-  default     = "PassThrough"
+  default     = "hello-nodejs-awssdk-test"
 }
 
 variable "architecture" {
   type        = string
   description = "Lambda function architecture, valid values are arm64 or x86_64"
   default     = "x86_64"
+}
+
+variable "application_name" {
+  type        = string
+  description = "Application attribute, expected key=value like application=YOUR_APPLICATION_NAME"
+  default     = "application=nodejs-application"
+}
+
+variable "service_name" {
+  type        = string
+  description = "Application service name"
+  default     = "nodejs-lambda-svc"
+}
+
+variable "traces_sampler" {
+  type        = string
+  description = "OpenTelemetry Traces Sampler configuration"
+  default     = "always_on"
+}
+
+variable "sumologic_http_traces_endpoint_url" {
+  type        = string
+  description = "Sumo Logic HTTP Traces URL"
 }
