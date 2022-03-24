cc1ed00b9edc4b0426b82c1400bd8e4bb29943e2
Mateusz Rumian <mrumian@sumologic.com>
2022-03-24 23:30:24 +0100

    chore(nodejs): try settracerprovider

--- nodejs/packages/layer/src/wrapper.ts
+++ nodejs/packages/layer/src/wrapper.ts
@@ -1,5 +1,8 @@
 import { env } from 'process';
-import { NodeTracerConfig, NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import {
+  NodeTracerConfig,
+  NodeTracerProvider,
+} from '@opentelemetry/sdk-trace-node';
 import {
   BatchSpanProcessor,
   ConsoleSpanExporter,
@@ -19,69 +22,63 @@ import {
   diag,
   DiagConsoleLogger,
   DiagLogLevel,
-} from "@opentelemetry/api";
+  TracerProvider,
+} from '@opentelemetry/api';
 import { getEnv } from '@opentelemetry/core';
 import { AwsLambdaInstrumentationConfig } from '@opentelemetry/instrumentation-aws-lambda';
 
 // Use require statements for instrumentation to avoid having to have transitive dependencies on all the typescript
 // definitions.
-const { AwsLambdaInstrumentation } = require('@opentelemetry/instrumentation-aws-lambda');
+const {
+  AwsLambdaInstrumentation,
+} = require('@opentelemetry/instrumentation-aws-lambda');
 const { DnsInstrumentation } = require('@opentelemetry/instrumentation-dns');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
-const { GraphQLInstrumentation } = require('@opentelemetry/instrumentation-graphql');
+const {
+  ExpressInstrumentation,
+} = require('@opentelemetry/instrumentation-express');
+const {
+  GraphQLInstrumentation,
+} = require('@opentelemetry/instrumentation-graphql');
 const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
 const { HapiInstrumentation } = require('@opentelemetry/instrumentation-hapi');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
+const {
+  IORedisInstrumentation,
+} = require('@opentelemetry/instrumentation-ioredis');
 const { KoaInstrumentation } = require('@opentelemetry/instrumentation-koa');
-const { MongoDBInstrumentation } = require('@opentelemetry/instrumentation-mongodb');
-const { MySQLInstrumentation } = require('@opentelemetry/instrumentation-mysql');
+const {
+  MongoDBInstrumentation,
+} = require('@opentelemetry/instrumentation-mongodb');
+const {
+  MySQLInstrumentation,
+} = require('@opentelemetry/instrumentation-mysql');
 const { NetInstrumentation } = require('@opentelemetry/instrumentation-net');
 const { PgInstrumentation } = require('@opentelemetry/instrumentation-pg');
-const { RedisInstrumentation } = require('@opentelemetry/instrumentation-redis');
+const {
+  RedisInstrumentation,
+} = require('@opentelemetry/instrumentation-redis');
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 
 declare global {
   // in case of downstream configuring span processors etc
-  function configureTracerProvider(tracerProvider: NodeTracerProvider): void
+  function configureTracerProvider(tracerProvider: NodeTracerProvider): void;
+  function setTracerProvider(tracerProvider: TracerProvider): void;
   function configureTracer(defaultConfig: NodeTracerConfig): NodeTracerConfig;
   function configureSdkRegistration(
     defaultSdkRegistration: SDKRegistrationConfig
   ): SDKRegistrationConfig;
-  function configureLambdaInstrumentation(config: AwsLambdaInstrumentationConfig): AwsLambdaInstrumentationConfig
+  function configureLambdaInstrumentation(
+    config: AwsLambdaInstrumentationConfig
+  ): AwsLambdaInstrumentationConfig;
 }
 
 console.log('Registering OpenTelemetry');
 
 // configure lambda logging
-const logLevel = getEnv().OTEL_LOG_LEVEL
-diag.setLogger(new DiagConsoleLogger(), logLevel)
-
-// Register instrumentation
-registerInstrumentations({
-  instrumentations: [
-    new AwsInstrumentation({
-      suppressInternalInstrumentation: true,
-    }),
-    new AwsLambdaInstrumentation(),
-    new DnsInstrumentation(),
-    new ExpressInstrumentation(),
-    new GraphQLInstrumentation(),
-    new GrpcInstrumentation(),
-    new HapiInstrumentation(),
-    new HttpInstrumentation(),
-    new IORedisInstrumentation(),
-    new KoaInstrumentation(),
-    new MongoDBInstrumentation(),
-    new MySQLInstrumentation(),
-    new NetInstrumentation(),
-    new PgInstrumentation(),
-    new RedisInstrumentation(),
-  ],
-});
+const logLevel = getEnv().OTEL_LOG_LEVEL;
+diag.setLogger(new DiagConsoleLogger(), logLevel);
 
 async function initializeProvider() {
-
   // setup cloud.account.id and faas.id attributes
   const AWS = require('aws-sdk');
   const { promisify } = require('util');
@@ -95,7 +92,7 @@ async function initializeProvider() {
   const provider = 'aws';
   const lambdaAwsRegion = env.AWS_REGION;
   const lambdaFunctionName = env.AWS_LAMBDA_FUNCTION_NAME;
-  const faasId = `arn:${ provider }:lambda:${ lambdaAwsRegion }:${ cloudAccountId }:function:${ lambdaFunctionName }`;
+  const faasId = `arn:${provider}:lambda:${lambdaAwsRegion}:${cloudAccountId}:function:${lambdaFunctionName}`;
 
   const additionalResources = new Resource({
     'cloud.account.id': cloudAccountId,
@@ -118,7 +115,7 @@ async function initializeProvider() {
 
   const tracerProvider = new NodeTracerProvider(config);
   if (typeof configureTracerProvider === 'function') {
-    configureTracerProvider(tracerProvider)
+    configureTracerProvider(tracerProvider);
   } else {
     // defaults
     tracerProvider.addSpanProcessor(
@@ -127,7 +124,9 @@ async function initializeProvider() {
   }
   // logging for debug
   if (logLevel === DiagLogLevel.DEBUG) {
-    tracerProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+    tracerProvider.addSpanProcessor(
+      new SimpleSpanProcessor(new ConsoleSpanExporter())
+    );
   }
 
   let sdkRegistrationConfig: SDKRegistrationConfig = {};
@@ -135,5 +134,30 @@ async function initializeProvider() {
     sdkRegistrationConfig = configureSdkRegistration(sdkRegistrationConfig);
   }
   tracerProvider.register(sdkRegistrationConfig);
+
+  setTracerProvider(tracerProvider);
+
+  // Register instrumentation
+  registerInstrumentations({
+    instrumentations: [
+      new AwsInstrumentation({
+        suppressInternalInstrumentation: true,
+      }),
+      new AwsLambdaInstrumentation(),
+      new DnsInstrumentation(),
+      new ExpressInstrumentation(),
+      new GraphQLInstrumentation(),
+      new GrpcInstrumentation(),
+      new HapiInstrumentation(),
+      new HttpInstrumentation(),
+      new IORedisInstrumentation(),
+      new KoaInstrumentation(),
+      new MongoDBInstrumentation(),
+      new MySQLInstrumentation(),
+      new NetInstrumentation(),
+      new PgInstrumentation(),
+      new RedisInstrumentation(),
+    ],
+  });
 }
 initializeProvider();
